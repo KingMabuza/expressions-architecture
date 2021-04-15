1f1205bbf9e9af7f8c0194c9c7d86fedf2e4e893
KingMabuza <amal@amallevi.com>
2021-04-15 14:30:06 +0200

    Third Commit

--- services.php
+++ services.php
@@ -61,7 +61,7 @@
 										<li><a href="index.php">Home</a></li>
 										<li><a href="about.php">About</a></li>
 										<li><a href="services.php">Services</a></li>
-										<li><a href="projects.php">Projects</a></li>
+										<li><a href="project.php">Projects</a></li>
                                         <li><a href="contact.php">Contact</a></li>
 									</ul>
 								</nav>
